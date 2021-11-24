2cef8046304e99f2df9df66a731ae08b9ff43ea5
Micha <micha.pdm@gmx.de>
2021-11-24 16:23:14 +0100

    feat: artificial data input is sent to backend

--- sensormock/src/App.js
+++ sensormock/src/App.js
@@ -4,36 +4,39 @@ import './App.css';
 
 function App() {
 
+  const [mockData, setMockData] = useState();
+  const [dateEpoch, setDateEpoch] = useState();
+  const [timeEpoch, setTimeEpoch] = useState();
+
   const sendTestData = (e) => {
-    // const [posts, setPosts] = useState();
-    // const [error, setError] = useState();
-    // const [userInput, setUserInput] = useState(mockDataInitial);
 
     const mockDataInitial = {
       "sensor_id": '1',
-      "time": 12345678,
+      "time": 1637756874,
       "pressure": 1021.659,
       "temperature": 25.88761,
       "humidity": 38,
-      "carbondioxide": 515,
-      "organic": 17,
-      "sensorData": [42.4242, 39]
+      // "carbondioxide": 515,
+      // "organic": 17,
+      // "sensorData": [42.4242, 39]
     };
 
-    const mockData = {
-      "sensor_id": '1',
-      "time": 12345678,
-      "pressure": 1021.659,
-      "temperature": 25.88761,
-      "humidity": 38,
-      "carbondioxide": 515,
-      "organic": 17,
-      "sensorData": [42.4242, 39]
-    };
+    // let mockData = {
+    //   "sensor_id": '1',
+    //   "time": 1637756874,
+    //   "pressure": 1021.659,
+    //   "temperature": 25.88761,
+    //   "humidity": 38,
+    //   // "carbondioxide": 515,
+    //   // "organic": 17,
+    //   // "sensorData": [42.4242, 39]
+    // };
 
     console.log("sendTestData");
     e.preventDefault();
     const apiUrl = "http://localhost:3000/sensor";
+
+    setMockData({time:  dateEpoch + timeEpoch});
 
     const options = {
       method: "POST",
@@ -60,12 +63,104 @@ function App() {
       });
   }
 
+  const handleInputChange = (event) => {
+    setMockData({ ...mockData, [event.target.name]: event.target.value })
+    console.log(mockData);
+  }
+
+  const handleDateChange = (event) => {
+    console.log(event.target.valueAsNumber);
+    setDateEpoch(event.target.valueAsNumber);
+  }
+
+  const handleTimeChange = (event) => {
+    console.log(event.target.valueAsNumber);
+    setTimeEpoch(event.target.valueAsNumber);
+  }
+
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={sendTestData}>Send Testdata</button>
+
+        <div className="inputContainer">
+          <form onSubmit={sendTestData}>
+            <div className="inputForm">
+              <label htmlFor="sensorID">Sensor-ID [#]</label>
+              <input className="inputField"
+                id="sensorID"
+                onChange={handleInputChange}
+                type="number"
+                name="sensor_id"
+                min="0"
+              />
+            </div>
+
+            <div className="inputForm">
+              <label htmlFor="time">Date [d]</label>
+              <input className="inputField"
+                id="time"
+                onChange={handleDateChange}
+                type="date"
+                name="time"
+                min="0"
+              />
+            </div>
+
+            <div className="inputForm">
+              <label htmlFor="time">Time [t]</label>
+              <input className="inputField"
+                id="time"
+                onChange={handleTimeChange}
+                type="time"
+                name="time"
+                min="0"
+              />
+            </div>
+
+            <div className="inputForm">
+              <label htmlFor="temperature">Temperature [°C]</label>
+              <input className="inputField"
+                id="temperature"
+                onChange={handleInputChange}
+                type="number"
+                name="temperature"
+                min="-100"
+                max="100"
+              />
+            </div>
+
+            <div className="inputForm">
+              <label htmlFor="pressure">Pressure [hPa]</label>
+              <input className="inputField"
+                id="pressure"
+                onChange={handleInputChange}
+                type="number"
+                name="pressure"
+                min="800"
+                max="1200"
+              />
+            </div>
+
+            <div className="inputForm">
+              <label htmlFor="humidity">Humidity [%]</label>
+              <input className="inputField"
+                id="humidity"
+                onChange={handleInputChange}
+                type="number"
+                name="humidity"
+                min="0"
+                max="100"
+              />
+            </div>
+
+            <div className="align-right">
+              <button>Send</button>
+            </div>
+          </form>
+
+        </div>
       </header>
     </div>
   );
