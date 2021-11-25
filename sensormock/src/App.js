4c03a42f25c98d8359425f8110dcaff509d5d6a2
Micha <micha.pdm@gmx.de>
2021-11-25 10:11:24 +0100

    feat: generate a bunch of data

--- sensormock/src/App.js
+++ sensormock/src/App.js
@@ -1,51 +1,80 @@
+
+/*
+  ToDo: after generating data and sending them it is not possible to press the button to generate
+  them again without changing the values to generate them
+*/
+
 import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+const generatorSettingInitial = {
+  toalNumberOfDatasets: 1,
+  timeInkrement: 1,
+}
+
+
+const TEMPERATUR_STEP = 1
+const PRESSURE_STEP = 10
+const HUMIDITY_STEP = 3
+
+
+const mockDataInitial = {
+  "sensor_id": '1',
+  "time": 1637756874,
+  "pressure": 1000,
+  "temperature": 20,
+  "humidity": 40,
+};
+
+let generatedDataArray = [{
+  "sensor_id": '1',
+  "time": 1637756874,
+  "pressure": 1000,
+  "temperature": 20,
+  "humidity": 40,
+}];
+
 function App() {
 
-  const [mockData, setMockData] = useState();
+  const [mockData, setMockData] = useState(mockDataInitial);
   const [dateEpoch, setDateEpoch] = useState();
   const [timeEpoch, setTimeEpoch] = useState();
+  const [generatorSetting, setGeneratorSetting] = useState(generatorSettingInitial);
+
+
 
   const sendTestData = (e) => {
-
-    const mockDataInitial = {
-      "sensor_id": '1',
-      "time": 1637756874,
-      "pressure": 1021.659,
-      "temperature": 25.88761,
-      "humidity": 38,
-      // "carbondioxide": 515,
-      // "organic": 17,
-      // "sensorData": [42.4242, 39]
-    };
-
-    // let mockData = {
-    //   "sensor_id": '1',
-    //   "time": 1637756874,
-    //   "pressure": 1021.659,
-    //   "temperature": 25.88761,
-    //   "humidity": 38,
-    //   // "carbondioxide": 515,
-    //   // "organic": 17,
-    //   // "sensorData": [42.4242, 39]
-    // };
 
     console.log("sendTestData");
     e.preventDefault();
     const apiUrl = "http://localhost:3000/sensor";
 
-    setMockData({time:  dateEpoch + timeEpoch});
+    setMockData({ time: dateEpoch + timeEpoch });
+    let options = {};
+    if (generatedDataArray.length > 1) {
+      options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include"
+        },
+        body: JSON.stringify(generatedDataArray)
+        // body: JSON.stringify(mockData)
+      }
+      generatedDataArray = generatedDataArray[0];
+    } else {
+      console.log("mockData");
+      options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include"
+        },
+        body: JSON.stringify(mockData)
+      };
+    }
 
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        credentials: "include"
-      },
-      body: JSON.stringify(mockData)
-    };
 
     fetch(apiUrl, options)
       .then((res) => {
@@ -64,20 +93,96 @@ function App() {
   }
 
   const handleInputChange = (event) => {
-    setMockData({ ...mockData, [event.target.name]: event.target.value })
+    setMockData({ ...mockData, [event.target.name]: Number(event.target.value) })
     console.log(mockData);
   }
 
   const handleDateChange = (event) => {
+    console.log(event.target)
     console.log(event.target.valueAsNumber);
-    setDateEpoch(event.target.valueAsNumber);
+    setDateEpoch(Number(event.target.valueAsNumber));
   }
 
   const handleTimeChange = (event) => {
-    console.log(event.target.valueAsNumber);
-    setTimeEpoch(event.target.valueAsNumber);
+    setTimeEpoch(event.target.valueAsNumber * 1000);
+    console.log(timeEpoch);
   }
 
+  const handleInputChangeGenerator = (event) => {
+    event.preventDefault();
+    setGeneratorSetting({ ...generatorSetting, [event.target.name]: event.target.value });
+    console.log(generatorSetting);
+
+  }
+
+
+  const randomwalk = (startvalue = 0, stepheight = 1) => {
+    const direction = (Math.floor(Math.random() * 3));
+    let nextStep = 0;
+    if (stepheight < 1) stepheight = 1;
+
+    switch (direction) {
+      case 0:
+        nextStep = startvalue;
+        break;
+      case 1:
+        nextStep = startvalue + stepheight;
+        break;
+      case 2:
+        nextStep = startvalue - stepheight;
+        break;
+      default:
+        return startvalue;
+    }
+    return nextStep;
+  }
+
+
+  const handleButtonChangeGenerator = (event) => {
+    event.preventDefault();
+
+    generatedDataArray = [{
+
+    }];
+
+    // if (!mockData) setMockData();
+
+    for (let n = 0; n < generatorSetting.toalNumberOfDatasets; n++) {
+      console.log(n * generatorSetting.timeInkrement)
+      let timeCalculated = (n * timeEpoch) + dateEpoch + timeEpoch;
+      // if (!timeEpoch) timeCalculated = ((n * 1000) + 1673712000000); // Not Working
+
+      let humidityTmp = 0;
+      let temperatureTmp = 0;
+      let pressureTmp = 0;
+      if (n === 0) {
+        console.log("A")
+        console.log(`mockData.humidity: ${mockData.humidity}`)
+        humidityTmp = randomwalk(mockData.humidity, HUMIDITY_STEP);
+        temperatureTmp = randomwalk(mockData.temperature, TEMPERATUR_STEP);
+        pressureTmp = randomwalk(mockData.pressure, PRESSURE_STEP);
+      } else {
+        console.log("B")
+        humidityTmp = randomwalk(generatedDataArray[n - 1].humidity, HUMIDITY_STEP);
+        temperatureTmp = randomwalk(generatedDataArray[n - 1].temperature, TEMPERATUR_STEP);
+        pressureTmp = randomwalk(generatedDataArray[n - 1].pressure, PRESSURE_STEP);
+      }
+
+
+      const oneDataElement = {
+        "sensor_id": '1',       // ToDo
+        "time": timeCalculated,
+        "pressure": pressureTmp,
+        "temperature": temperatureTmp,
+        "humidity": humidityTmp,
+      };
+
+      if (n === 0) generatedDataArray[0] = oneDataElement
+      else generatedDataArray.push(oneDataElement);
+    }
+
+    console.log(generatedDataArray);
+  }
 
 
   return (
@@ -94,6 +199,7 @@ function App() {
                 type="number"
                 name="sensor_id"
                 min="0"
+                value={mockData.sensor_id}
               />
             </div>
 
@@ -105,6 +211,7 @@ function App() {
                 type="date"
                 name="time"
                 min="0"
+                value={dateEpoch}
               />
             </div>
 
@@ -128,6 +235,7 @@ function App() {
                 name="temperature"
                 min="-100"
                 max="100"
+                value={mockData.temperature}
               />
             </div>
 
@@ -140,6 +248,7 @@ function App() {
                 name="pressure"
                 min="800"
                 max="1200"
+                value={mockData.pressure}
               />
             </div>
 
@@ -152,11 +261,44 @@ function App() {
                 name="humidity"
                 min="0"
                 max="100"
+                value={mockData.humidity}
               />
             </div>
 
             <div className="align-right">
               <button>Send</button>
+            </div>
+          </form>
+
+        </div>
+        < hr />
+        <div className="inputContainer">
+          <form onSubmit={handleButtonChangeGenerator}>
+
+            <div className="inputForm">
+              <label htmlFor="toalNumberOfDatasets">Number of Datasets [n]</label>
+              <input className="inputField"
+                id="toalNumberOfDatasets"
+                onChange={handleInputChangeGenerator}
+                type="number"
+                name="toalNumberOfDatasets"
+                min="0"
+                max="100"
+              />
+
+              <label htmlFor="timeInkrement">Timeinkrement [s]</label>
+              <input className="inputField"
+                id="timeInkrement"
+                onChange={handleTimeChange}
+                type="number"
+                name="timeInkrement"
+                min="0"
+                max="3600"
+              />
+            </div>
+
+            <div className="align-right">
+              <button>Generate</button>
             </div>
           </form>
 
